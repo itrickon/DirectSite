a46577d9587e4e6f6ac57ede32deab1effcfd9a0
Zakhar Itricko <erroraccount@bk.ru>
2026-03-23 12:41:39 +0400

    Исправлено логирование для Render (только консоль)

--- directsite/directsite/settings.py
+++ directsite/directsite/settings.py
@@ -180,12 +180,6 @@ LOGGING = {
         },
     },
     'handlers': {
-        'file': {
-            'level': 'INFO',
-            'class': 'logging.FileHandler',
-            'filename': BASE_DIR / 'logs' / 'django.log',
-            'formatter': 'verbose',
-        },
         'console': {
             'level': 'DEBUG',
             'class': 'logging.StreamHandler',
@@ -194,12 +188,12 @@ LOGGING = {
     },
     'loggers': {
         'django': {
-            'handlers': ['file', 'console'],
+            'handlers': ['console'],
             'level': 'INFO',
             'propagate': True,
         },
         'core': {
-            'handlers': ['file', 'console'],
+            'handlers': ['console'],
             'level': 'INFO',
             'propagate': False,
         },
